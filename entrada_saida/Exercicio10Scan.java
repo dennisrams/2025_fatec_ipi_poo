60e42806359f679145f0dfd21d8d43a14c7d3693
Dennis Ramos <dennisxramos@gmail.com>
2025-03-06 07:58:10 -0300

    feat: Adição do sistema de inserção do valor do veículo

--- entrada_saida/Exercicio10Scan.java
+++ entrada_saida/Exercicio10Scan.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Exercicio10Scan{
     static public void main(String[] abc){
         
-        int ValorCarro = 15000;
+        double ValorCarro;
 
-        int PorcentagemDistruibuidor = ValorCarro * 28 / 100;
-        int PorcentagemImpostos = ValorCarro * 45 / 100;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor do carro: ");
+        ValorCarro = scanner.nextDouble();
+        
+        double PorcentagemDistruibuidor = ValorCarro * 28 / 100;
+        double PorcentagemImpostos = ValorCarro * 45 / 100;
 
         ValorCarro = ValorCarro + PorcentagemDistruibuidor + PorcentagemImpostos;
 
