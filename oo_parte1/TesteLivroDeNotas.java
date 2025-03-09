98720b60426ba59278b9b1fd1a9fb921dcb1eb86
Dennis Ramos <dennisxramos@gmail.com>
2025-03-08 21:09:28 -0300

    feat: correção do código, removendo os "=" que vinha após o setNomeDaDisciplina

--- oo_parte1/TesteLivroDeNotas.java
+++ oo_parte1/TesteLivroDeNotas.java
@@ -1,11 +1,14 @@
+
 public class TesteLivroDeNotas{
     public static void main(String...args){
+
         //1. Construir o livro de notas
         LivroDeNotas livroDeNotas = new LivroDeNotas();
         LivroDeNotas livro2 = new LivroDeNotas();
+
         //livroDeNotas.nomeDaDisciplina = "POO";
-        livroDeNotas.setNomeDaDisciplina = ("POO");
-        livro2.nomeDaDisciplina = "BD";
+        livroDeNotas.setNomeDaDisciplina("POO");
+        livro2.setNomeDaDisciplina("BD");
 
         //2. Acionar o comportamento dele de exibir a mensagem de boas vindas
         livroDeNotas.exibirMensagem();
